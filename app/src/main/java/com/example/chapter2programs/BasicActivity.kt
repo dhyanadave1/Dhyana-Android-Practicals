bd929ebddc031909729747d3118d6b92570b2736
Dhyana Dave <dhyana.d@simformsolutions.com>
2022-04-05 12:53:00 +0530

    Android Layouts

---
+++ app/src/main/java/com/example/chapter2programs/BasicActivity.kt
@@ -0,0 +1,18 @@
+package com.example.chapter2programs
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_basic.btnCh2
+
+class BasicActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_basic)
+
+        btnCh2.setOnClickListener {
+            val intent = Intent(this@BasicActivity, MainLayoutActivity :: class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/chapter2programs/FrameLayoutActivity.kt
@@ -0,0 +1,17 @@
+package com.example.chapter2programs
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_table_layout.btnFeedback
+
+class FrameLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frame_layout)
+
+        btnFeedback.setOnClickListener() {
+            Toast.makeText(this@FrameLayoutActivity, getString(R.string.feedbacksub), Toast.LENGTH_SHORT).show()
+        }
+    }
+}
